3390575a80ef50496fce8ed9eee675e6647807b9
Isaias de Jesus Barros <isaiasjbarross@gmail.com>
2022-10-23 15:06:29 -0400

    Exercicio Manipulando String

---
+++ String Manipulacao/Program.cs
@@ -0,0 +1,49 @@
+﻿
+/// Validar Nome, os 3 primeiros caracteres devem ser obrigatoriamente letras em  seguida espaço e pelo menos um número
+
+// ex ABC 1ab
+// letra.letra.letra.[espaco].numeros
+
+
+
+string nome;
+bool temEspacoAposLetras = false;
+bool nomeValido = false;
+
+Console.Clear();
+
+Console.Write("Digite seu nome: ");
+ nome = Console.ReadLine().Replace("-", ""); // evitar numeros negativos
+
+for (int i = 0; i < nome.Length; i++)
+{
+  for (int j = 0; j <= 9; j++)
+  {
+      string num = j.ToString();
+
+      if((i < 3 && nome[i].ToString() == num)){ // verifica se os 3 primeiros digitos sao apenas letras
+        System.Console.WriteLine("Deve haver apenas letras nos 3 primeiros caracteres");
+        return;
+      }
+
+      if( (nome[3].ToString() == " " && temEspacoAposLetras == false)){ //verifica se tem espaco apos os 3 primeiros digitos
+        temEspacoAposLetras = true;
+      }
+
+      if((temEspacoAposLetras == true && nome[i].ToString() == num) && nomeValido == false){
+          nomeValido = true; // caso haja espaco appos as 3 primeiras letras, entra aqui e verifica se tem numeros apos o espaco ou dps
+      }
+    }
+
+    if(nomeValido == true){
+      System.Console.WriteLine("Nome Válido");
+      return;
+    }
+
+    if(i == nome.Length-1 && nomeValido == false){ 
+      
+      System.Console.WriteLine("Nome Inválido");
+      return;
+    }
+    
+  }
---
+++ String Manipulacao/README.md
@@ -0,0 +1,7 @@
+### Exercicio manipulando string :books:
+
+
+```
+O Exercicio em si, tem que deixar cadastrar um nome que contenha 3 letras iniciais, não podendo se numero ou outra coisa, logo após   
+as 3 letras te que conter espaco e depois numeros, para validar o nome.
+```
